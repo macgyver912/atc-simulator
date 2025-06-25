643c29685bc8be12b42eaf9049707f7196ab7ae2
Jaime Valle Alonso <j.valleal@alumnos.urjc.es>
2025-06-25 16:45:41 +0200

    [FIX] Reestablecidas coordenadas de radioayudas en DMS por mala interpretación anterior, se pensaba que eran DD pero son DMS.

--- Assets/Scripts/game/CreateObjects.cs
+++ Assets/Scripts/game/CreateObjects.cs
@@ -50,24 +50,21 @@ public class CreateObjects : MonoBehaviour
         // Runways
         Runway[] rwys = new Runway[8];
         
-        rwys[0] = new Runway("14R", 143, 13083, 40.290550f, -3.343364f, 2000f, false);
-        rwys[1] = new Runway("32L", 323, 13083, 40.274710f, -3.331402f, 2000f, true);
+        rwys[0] = new Runway("14R", 143, 13083, Measurement.DMS2DD(40, 29, 05.50f), Measurement.DMS2DD(-3, 34, 33.64f), 2000f, false);
+        rwys[1] = new Runway("32L", 323, 13083, Measurement.DMS2DD(40, 27, 47.10f), Measurement.DMS2DD(-3, 33, 14.02f), 2000f, true);
 
-        rwys[2] = new Runway("14L", 143, 11482, 40.294171f, -3.332833f, 2000f, false);
-        rwys[3] = new Runway("32R", 323, 11482, 40.282485f, -3.321030f, 2000f, true);
+        rwys[2] = new Runway("14L", 143, 11482, Measurement.DMS2DD(40, 29, 41.71f), Measurement.DMS2DD(-3, 33, 28.33f), 2000f, false);
+        rwys[3] = new Runway("32R", 323, 11482, Measurement.DMS2DD(40, 28, 24.85f), Measurement.DMS2DD(-3, 32, 10.30f), 2000f, true);
 
-        rwys[4] = new Runway("36L", 360, 11482, 40.293332f, -3.342864f, 2000f, false);
-        rwys[5] = new Runway("18R", 180, 11482, 40.312240f, -3.342927f, 2000f, true);
+        rwys[4] = new Runway("36L", 360, 11482, Measurement.DMS2DD(40, 29, 33.32f), Measurement.DMS2DD(-3, 34, 28.64f), 2000f, false);
+        rwys[5] = new Runway("18R", 180, 11482, Measurement.DMS2DD(40, 31, 22.40f), Measurement.DMS2DD(-3, 34, 29.27f), 2000f, true);
 
-        rwys[6] = new Runway("36R", 360, 11482, 40.300397f, -3.333315f, 2000f, false);
-        rwys[7] = new Runway("18L", 180, 11482, 40.314122f, -3.333368f, 2000f, true);
+        rwys[6] = new Runway("36R", 360, 11482, Measurement.DMS2DD(40, 30, 03.97f), Measurement.DMS2DD(-3, 33, 33.15f), 2000f, false);
+        rwys[7] = new Runway("18L", 180, 11482, Measurement.DMS2DD(40, 31, 41.22f), Measurement.DMS2DD(-3, 33, 33.68f), 2000f, true);
 
 
         // Airport
-        //airport = new Airport("Madrid-Barajas", "LEMD", "MAD", Measurement.DMS2DD(40, 28, 20f), Measurement.DMS2DD(-3, 33, 39f), 2000f, "Madrid", "Spain", 13000, 140, rwys);
-        airport = new Airport("Madrid-Barajas", "LEMD", "MAD", 40.2820f, -3.3339f, 2000f, "Madrid", "Spain", 13000, 140, rwys);
-
-
+        airport = new Airport("Madrid-Barajas", "LEMD", "MAD", Measurement.DMS2DD(40, 28, 20f), Measurement.DMS2DD(-3, 33, 39f), 2000f, "Madrid", "Spain", 13000, 140, rwys);
 
 
         fixList = new Dictionary<string, FIX>();
@@ -90,45 +87,42 @@ public class CreateObjects : MonoBehaviour
         */
 
         // LEMD - STAR 3 RNAV
-        fixList.Add("AVILA", new FIX("AVILA", 40.37286f, -4.32596f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("BUREX", new FIX("BUREX", 39.48398f, -3.56215f, FIX.FixTypes.Compulsory, true));
-        fixList.Add("DAQSE", new FIX("DAQSE", 40.20351f, -4.08481f, FIX.FixTypes.Compulsory, true));
-        fixList.Add("FAFEQ", new FIX("FAFEQ", 40.10098f, -3.27385f, FIX.FixTypes.Compulsory, true));
-        fixList.Add("MORAL", new FIX("MORAL", 39.00000f, -3.32318f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("NONTU", new FIX("NONTU", 41.30011f, -4.10084f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("ORBIS", new FIX("ORBIS", 41.15566f, -4.11432f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("RIDAV", new FIX("RIDAV", 40.32069f, -5.48298f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("SOTUK", new FIX("SOTUK", 39.11372f, -4.44470f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("URRIF", new FIX("URRIF", 40.14323f, -3.44467f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("YUNYE", new FIX("YUNYE", 40.02387f, -3.37442f, FIX.FixTypes.OnRequest, true));
+        fixList.Add("AVILA", new FIX("AVILA", Measurement.DMS2DD(40, 37, 28.6f), Measurement.DMS2DD(-4, 32, 59.6f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("BUREX", new FIX("BUREX", Measurement.DMS2DD(39, 48, 39.8f), Measurement.DMS2DD(-3, 56, 21.5f), FIX.FixTypes.Compulsory, true));
+        fixList.Add("DAQSE", new FIX("DAQSE", Measurement.DMS2DD(40, 20, 35.1f), Measurement.DMS2DD(-4, 08, 48.1f), FIX.FixTypes.Compulsory, true));
+        fixList.Add("FAFEQ", new FIX("FAFEQ", Measurement.DMS2DD(40, 10, 09.8f), Measurement.DMS2DD(-3, 27, 38.5f), FIX.FixTypes.Compulsory, true));
+        fixList.Add("MORAL", new FIX("MORAL", Measurement.DMS2DD(39, 00, 00.0f), Measurement.DMS2DD(-3, 32, 31.8f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("NONTU", new FIX("NONTU", Measurement.DMS2DD(41, 30, 01.1f), Measurement.DMS2DD(-4, 10, 08.4f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("ORBIS", new FIX("ORBIS", Measurement.DMS2DD(41, 15, 56.6f), Measurement.DMS2DD(-4, 11, 43.2f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("RIDAV", new FIX("RIDAV", Measurement.DMS2DD(40, 32, 06.9f), Measurement.DMS2DD(-5, 48, 29.8f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("SOTUK", new FIX("SOTUK", Measurement.DMS2DD(39, 11, 37.2f), Measurement.DMS2DD(-4, 44, 47.0f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("URRIF", new FIX("URRIF", Measurement.DMS2DD(40, 14, 32.3f), Measurement.DMS2DD(-3, 44, 46.7f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("YUNYE", new FIX("YUNYE", Measurement.DMS2DD(40, 02, 38.7f), Measurement.DMS2DD(-3, 37, 44.2f), FIX.FixTypes.OnRequest, true));
 
-        
-        //vorList.Add("ZMR", new VOR("ZMR", "Zamora", 0.0f, null, true, 41.31482f, -5.38231f));
+
+        //vorList.Add("ZMR", new VOR("ZMR", "Zamora", 0.0f, null, true, Measurement.DMS2DD(41, 31, 48.2f), Measurement.DMS2DD(-5, 38, 23.1f)));
 
 
 
 
         // LEMD - STAR 4 RNAV
-        fixList.Add("ADUXO", new FIX("ADUXO", 40.30444f, -2.03514f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("BANEV", new FIX("BANEV", 41.30094f, -2.30523f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("RUDBI", new FIX("RUDBI", 40.15294f, -3.08100f, FIX.FixTypes.Compulsory, true));
-        fixList.Add("NOSKO", new FIX("NOSKO", 40.39228f, -2.49002f, FIX.FixTypes.Compulsory, true));
-        fixList.Add("PINAR", new FIX("PINAR", 40.58491f, -2.35570f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("PRADO", new FIX("PRADO", 40.08510f, -2.00372f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("SIRGU", new FIX("SIRGU", 40.15378f, -2.36005f, FIX.FixTypes.Compulsory, true));
-        fixList.Add("TERSA", new FIX("TERSA", 40.43301f, -2.08162f, FIX.FixTypes.OnRequest, true));
-        fixList.Add("VILLA", new FIX("VILLA", 40.13586f, -2.24376f, FIX.FixTypes.OnRequest, true));
+        fixList.Add("ADUXO", new FIX("ADUXO", Measurement.DMS2DD(40, 30, 44.4f), Measurement.DMS2DD(-2, 03, 51.4f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("BANEV", new FIX("BANEV", Measurement.DMS2DD(41, 30, 09.4f), Measurement.DMS2DD(-2, 30, 52.3f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("RUDBI", new FIX("RUDBI", Measurement.DMS2DD(40, 15, 29.4f), Measurement.DMS2DD(-3, 08, 10.0f), FIX.FixTypes.Compulsory, true));
+        fixList.Add("NOSKO", new FIX("NOSKO", Measurement.DMS2DD(40, 39, 22.8f), Measurement.DMS2DD(-2, 49, 00.2f), FIX.FixTypes.Compulsory, true));
+        fixList.Add("PINAR", new FIX("PINAR", Measurement.DMS2DD(40, 58, 49.1f), Measurement.DMS2DD(-2, 35, 57.0f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("PRADO", new FIX("PRADO", Measurement.DMS2DD(40, 08, 51.0f), Measurement.DMS2DD(-2, 00, 37.2f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("SIRGU", new FIX("SIRGU", Measurement.DMS2DD(40, 15, 37.8f), Measurement.DMS2DD(-2, 36, 00.5f), FIX.FixTypes.Compulsory, true));
+        fixList.Add("TERSA", new FIX("TERSA", Measurement.DMS2DD(40, 43, 30.1f), Measurement.DMS2DD(-2, 08, 16.2f), FIX.FixTypes.OnRequest, true));
+        fixList.Add("VILLA", new FIX("VILLA", Measurement.DMS2DD(40, 13, 58.6f), Measurement.DMS2DD(-2, 24, 37.6f), FIX.FixTypes.OnRequest, true));
 
         // LEMD - VOR   
-        vorList.Add("SSY", new VOR("SSY", "San Sebastian de los Reyes", 117.85f, "... ... -.--", true, 40.3247f, -3.3431f, false));
-        vorList.Add("BRA", new VOR("BRA", "Barajas", 116.45f, "-... .-. .-", true, 40.2809f, -3.3327f, false));
-        vorList.Add("CNR", new VOR("CNR", "Colmenar Viejo", 117.30f, "-.-. -. .-.", true, 40.3846f, -3.4409f, true));
-        //vorList.Add("PDTx", new VOR("PDTx", "Perales", 116.75f, ".--. -.. -", true, Measurement.DMS2DD(40, 15, 10f), Measurement.DMS2DD(-3, 20, 52f), true));
-        vorList.Add("PDT", new VOR("PDT", "Perales", 116.75f, ".--. -.. -", true, 40.1510f, -3.2053f, true));
-        vorList.Add("TLD", new VOR("TLD", "Toledo", 113.20f, "- .-.. -..", true, 39.58100f, -4.2015f, true));
-        vorList.Add("RBO", new VOR("RBO", "Robledillo", 113.95f, ".-. -... ---", true, 40.51143f, -3.14474f, true));
-
-
+        vorList.Add("SSY", new VOR("SSY", "San Sebastian de los Reyes", 117.85f, "... ... -.--", true, Measurement.DMS2DD(40, 32, 47.1f), Measurement.DMS2DD(-3, 34, 30.7f), false));
+        vorList.Add("BRA", new VOR("BRA", "Barajas", 116.45f, "-... .-. .-", true, Measurement.DMS2DD(40, 28, 08.9f), Measurement.DMS2DD(-3, 33, 27.1f), false));
+        vorList.Add("CNR", new VOR("CNR", "Colmenar Viejo", 117.30f, "-.-. -. .-.", true, Measurement.DMS2DD(40, 38, 45.5f), Measurement.DMS2DD(-3, 44, 09.0f), true));
+        vorList.Add("PDT", new VOR("PDT", "Perales", 116.75f, ".--. -.. -", true, Measurement.DMS2DD(40, 15, 10f), Measurement.DMS2DD(-3, 20, 52f), true));
+        vorList.Add("TLD", new VOR("TLD", "Toledo", 113.20f, "- .-.. -..", true, Measurement.DMS2DD(39, 58, 10.1f), Measurement.DMS2DD(-4, 20, 14.6f), true));
+        vorList.Add("RBO", new VOR("RBO", "Robledillo", 113.95f, ".-. -... ---", true, Measurement.DMS2DD(40, 51, 13.9f), Measurement.DMS2DD(-3, 14, 47.9f), true));
 
         // 	vorList.Add(new VOR("INV", "Inventado", 116.75, null, false, Measurement.DMS2DD(40, 00, 00), Measurement.DMS2DD(-7, 00, 00), 0));
         // 	vorList.Add(new VOR("INV", "Inventado", 116.75, null, false, Measurement.DMS2DD(40, 00, 00), Measurement.DMS2DD(-3, 33, 39)+3.439167, 0));
